d77de4abdd41f291bc4ef9eceebd1a91ff92853f
Michelle <michgabz54@gmail.com>
2023-09-07 15:03:12 +0800

    first commit

---
+++ door.js
@@ -0,0 +1,11 @@
+function openDoor(){
+    document.getElementById("imgStatus").src = "open.jpg";
+    document.getElementById("doorStatus").innerHTML = "Marry Chistmas!";
+    document.getElementById('mp3File').play();
+}
+function closeDoor(){
+    document.getElementById("imgStatus").src = "Images/close.png";
+    document.getElementById("doorStatus").innerHTML = "The Door is CLOSE!";
+    document.getElementById('mp3File').pause();
+    document.getElementById('mp3File').currentTime = 0;
+}
